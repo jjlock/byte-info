a5495cb7413a3387021c88d5c72c5ca486eb7a89
Justin Lock <jjlock@uci.edu>
2020-12-24 17:31:35 -0800

    Add ability to gracefully shutdown the server

--- main.go
+++ main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/jjlock/byte-scraper-api/handler"
 )
 
 func main() {
-	srvHandler := handler.NewScraperHandler()
+	srv := &http.Server{
+		Addr:         ":8000",
+		Handler:      handler.NewScraperHandler(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
 
-	log.Fatal(http.ListenAndServe(":8000", srvHandler))
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// start server in goroutine so it does not block
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen: %v", err)
+		}
+	}()
+	log.Printf("Server started on localhost%s", srv.Addr)
+
+	<-done
+	log.Print("Server stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failed: %v", err)
+	}
+
+	log.Print("Server shutdown gracefully")
 }
